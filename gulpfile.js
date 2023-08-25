e8361501e6d89541f3d0e457e60219b11991261d
ShirayDan <dan.shirayy@gmail.com>
2023-08-25 10:00:20 +0300

    innitial commit

---
+++ gulpfile.js
@@ -0,0 +1,16 @@
+const { src, dest, watch, series } = require('gulp')
+const sass = require('gulp-sass')(require('sass'))
+const purgecss = require('gulp-purgecss')
+
+function buildStyles() {
+	return src('scss/**/*.scss')
+		.pipe(sass({ outputStyle: 'compressed' }))
+		.pipe(purgecss({ content: ['*.html'] }))
+		.pipe(dest('css'))
+}
+
+function watchTask() {
+	watch(['scss/**/*.scss'], buildStyles)
+}
+
+exports.default = series(buildStyles, watchTask)
